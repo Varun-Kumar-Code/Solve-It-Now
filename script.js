d11bb159a20fcbb82adbaa1c4f39557279af4d27
Varun Kumar <varunkumar1329@gmail.com>
2025-03-01 15:56:51 +0530

    commit it

--- script.js
+++ script.js
@@ -48,8 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-
 // Solve Math Problem
 document.getElementById('solve-problem').addEventListener('click', async () => {
   const problem = document.getElementById('math-input').value;
