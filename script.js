e7c445141832f071483c376775e1d2d4eb88b5cb
Varun Kumar <varunkumar1329@gmail.com>
2025-03-01 15:52:55 +0530

    commit it

--- script.js
+++ script.js
@@ -40,10 +40,6 @@ themeToggle.addEventListener('click', () => {
 
 
 
-
-
-
-
 // Generate Math Problem
 document.getElementById('generate-problem').addEventListener('click', async () => {
   const prompt = "Generate a random math problem (algebra, calculus, or geometry).";
